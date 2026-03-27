785cbfd094242cdf9ea0aa00b0f18038e88fa11b
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-03-28 01:21:30 +0530

    Add program to calculate the sum of two numbers

---
+++ Adition_of_two_Nums.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+  int a, b;
+cout<< "Enter the 1st number: " << endl;
+cin>>a;
+cout<< "Enter the 2nd number: " << endl;
+cin>> b;
+int sum = a + b;
+cout<< "Sum = " << sum;
+return 0;
+}

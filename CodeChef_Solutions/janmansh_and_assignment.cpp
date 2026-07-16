96dd114aac3ddf12ca9f403f6a292150c2897b9b
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-07-17 00:49:10 +0530

    Add solution for JASSIGNMENTS problem
    Implement solution for JASSIGNMENTS problem on CodeChef.

---
+++ CodeChef_Solutions/janmansh_and_assignment.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/JASSIGNMENTS
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, x;
+    cin>>t;
+    while(t--) {
+        cin>>x;
+        if(x<=7) {
+            cout<<"YES" << endl;
+        }
+        else {
+            cout<< "NO" << endl;
+        }
+    }
+}

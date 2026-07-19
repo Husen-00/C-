0e7c2a9c8ac59f26ae3b419395b6911ef7ec3f6a
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-07-19 23:38:28 +0530

    Create chef_in_office.cpp

---
+++ CodeChef_Solutions/chef_in_office.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/OFFICE
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t,x,y;
+    cin>> t;
+    while(t--) {
+        cin>>x;
+        cin>>y;
+        cout<<4*x+y<<endl;
+    }
+    return 0;
+}

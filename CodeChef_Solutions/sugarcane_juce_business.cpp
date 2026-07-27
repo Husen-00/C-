122fe331ab0bd4a03bcdbcb7d498f98c74bd8941
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-07-27 15:15:38 +0530

    Add solution for sugarcane juice business problem
    Implement sugarcane juice business logic for CodeChef problem.

---
+++ CodeChef_Solutions/sugarcane_juce_business.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/SUGARCANE
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t, n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        cout<<15*n<<endl;
+    }
+    return 0;
+}

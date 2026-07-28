7cc1bfd778ffdf0bfb64f885970dc7fae181349c
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-07-28 22:43:12 +0530

    Add solution for CodeChef problem NOTEBOOK
    Implement solution for CodeChef problem NOTEBOOK.

---
+++ CodeChef_Solutions/count_notebook.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/NOTEBOOK
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t, n;
+    cin>>t;
+    while(t--) {
+        cin>>n;
+        cout<<10*n<<endl;
+    }
+    return 0;
+}

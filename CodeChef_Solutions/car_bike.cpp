52d2d2cace721d35b8a671257d5216c0680280ac
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-07-30 10:54:01 +0530

    Add car_bike.cpp for travel cost comparison
    Implement solution for travel problem comparing bike and car costs.

---
+++ CodeChef_Solutions/car_bike.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/TRAVELFAST
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t, x, y;
+    cin>>t;
+    while(t--) {
+        cin>>x>>y;
+        if(x<y) {
+            cout<<"BIKE"<<endl;
+        }
+        else if(x>y) {
+            cout<<"CAR"<< endl;
+        }
+        else {
+            cout<<"SAME"<<endl;
+        }
+    }
+    return 0;
+}

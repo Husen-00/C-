9357228ae768f9757ca2558081909dd536e84d5c
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-08-01 22:32:54 +0530

    Add solution for CodeChef problem THREETOPICS

---
+++ CodeChef_Solutions/tree_topics.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/THREETOPICS
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a,b,c,x;
+    cin>>a>>b>>c>>x;
+    if(x==a || x==b || x==c) {
+        cout<<"YES\n";
+    }
+    else {
+        cout<<"NO\n";
+    }
+    return 0;
+}

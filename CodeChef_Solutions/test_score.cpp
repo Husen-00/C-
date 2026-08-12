09bbacf58e1fc7b7ff3721dd94217f08232da83d
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-08-12 23:44:46 +0530

    Add test_score.cpp for CHEFSCORE problem
    Implement solution for CodeChef problem CHEFSCORE.

---
+++ CodeChef_Solutions/test_score.cpp
@@ -0,0 +1,18 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CHEFSCORE
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t,n,x,y;
+    cin>>t;
+    while(t--) {
+        cin>>n>>x>>y;
+        if(y%x==0 && (y/x)<=n) {
+            cout<<"YES\n";
+        }
+        else {
+            cout<<"NO\n";
+        }
+    }
+    return 0;
+}

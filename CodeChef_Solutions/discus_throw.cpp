0e3ce4d1a97c655c34890b848faace39ff4f8036
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-08-15 20:25:35 +0530

    Implement discus throw solution in C++

---
+++ CodeChef_Solutions/discus_throw.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/DISCUS
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t,a,b,c;
+    cin>>t;
+    while(t--) {
+        cin>>a>>b>>c;
+        if(a>=b && a>=c) {
+            cout<<a<<endl;
+        }
+        else if(b>=a && b>=c) {
+            cout<<b<<endl;
+        }
+        else {
+            cout<<c<<endl;
+        }
+    }
+    return 0;
+}

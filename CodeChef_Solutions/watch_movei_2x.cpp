78affd3ade338599fb91b3cb187a50e841d8a02a
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-08-17 23:40:40 +0530

    Add movie watching time calculation
    Implement main function to calculate movie watching time.

---
+++ CodeChef_Solutions/watch_movei_2x.cpp
@@ -0,0 +1,8 @@
+//https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/MOVIE2X
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x,y;
+    cin>>x>>y;
+    cout<<(x-y) + (y/2);
+}

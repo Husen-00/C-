1f31adfafeecdeae448da1b88c9fbc13721089f7
Husensab Nadaf <husensabnadaf658@gmail.com>
2026-09-19 22:49:54 +0530

    Create second_largest.cpp

---
+++ CodeChef_Solutions/second_largest.cpp
@@ -0,0 +1,20 @@
+// https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/FLOW017
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int t, a, b, c; 
+    cin>>t;
+    while(t--) {
+        cin>>a>>b>>c;
+        if((a>b && a<c) || (a<b && a>c)) {
+            cout<<a<<endl;
+        }
+        else if((b>a&& b<c) || (b<a&&b>c)) {
+            cout<<b<<endl;
+        }
+        else {
+            cout<<c<<endl;
+        }
+    }
+    return 0;
+}
